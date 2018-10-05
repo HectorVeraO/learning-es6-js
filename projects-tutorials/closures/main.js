1a22050df67336d1faf5bab905864c2edbfc402e
Hector_Vera <juniomilnovecientos@gmail.com>
2018-10-05 13:02:38 -0300

    Closures added

---
+++ projects-tutorials/closures/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let name = "Héctor Vera";
+function greetMe() {
+  console.log("Hello,", name, "!");
+}
+
+name = "Felipe Vera";
+
+greetMe(); // prints Hello, Felipe vera !
+
+name = "Juan Vera";
